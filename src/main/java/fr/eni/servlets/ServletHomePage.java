aa1f4f18940a8cbd993a9695a34b6ddc368d6859
RaphaelStAlme <raphael_35_974@outlook.fr>
2021-09-07 16:53:22 +0200

    feat: added home page jsp and servlet (ewen).

---
+++ src/main/java/fr/eni/servlets/ServletHomePage.java
@@ -0,0 +1,20 @@
+package fr.eni.servlets;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+
+@WebServlet(name = "home", value = "/home")
+public class ServletHomePage extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/home.jsp");
+        rd.forward(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doGet(request, response);
+    }
+}
