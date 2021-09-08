bb869e8299d4f13dbf5e5e83254074b0c6b6b296
agathevrignaud <vrignaud.agathe@gmail.com>
2021-09-08 15:27:18 +0200

    ÃRemoved useless readonly (bad idea)

--- src/main/java/fr/eni/servlets/ServletDisplayProfile.java
+++ src/main/java/fr/eni/servlets/ServletDisplayProfile.java
@@ -1,6 +1,7 @@
 package fr.eni.servlets;
 
 import fr.eni.bll.UtilisateurManager;
+import fr.eni.bo.Utilisateur;
 
 import javax.servlet.*;
 import javax.servlet.http.*;
@@ -12,10 +13,11 @@ public class ServletDisplayProfile extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
+        //  TODO : Ajouter dynamiquement l'Id de l'utilisateur (pour l'instant sur 2 pour prouver que ça marche)
         UtilisateurManager utilisateurManager = new UtilisateurManager();
-        utilisateurManager.getUserById(2);
+        Utilisateur lUtilisateur = utilisateurManager.getUserById(2);
 
-
+        request.setAttribute("userInfo", lUtilisateur);
 
         RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/profileInformation.jsp");
         rd.forward(request, response);
