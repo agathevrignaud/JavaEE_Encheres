351811e1b52910db9a2414b0778ecbe2054a1aaa
agathevrignaud <vrignaud.agathe@gmail.com>
2021-09-08 10:57:49 +0200

    Added createArticle in ArticleVendu DAL

--- src/main/java/fr/eni/dal/ArticleVenduDAO.java
+++ src/main/java/fr/eni/dal/ArticleVenduDAO.java
@@ -1,8 +1,10 @@
 package fr.eni.dal;
 
+import fr.eni.bll.ArticleVenduManager;
 import fr.eni.bo.ArticleVendu;
 import java.util.List;
 
 public interface ArticleVenduDAO {
     public List<ArticleVendu> selectAll();
+    public void createArticle(ArticleVendu lArticle);
 }
