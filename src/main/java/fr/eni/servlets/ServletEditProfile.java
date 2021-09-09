ed6849b93910f2efbeb5a7b848e25b472a17eedc
agathevrignaud <vrignaud.agathe@gmail.com>
2021-09-09 08:57:21 +0200

    Fixed buttons + pom.xml + servletEditProfile

--- src/main/java/fr/eni/servlets/ServletEditProfile.java
+++ src/main/java/fr/eni/servlets/ServletEditProfile.java
@@ -1,5 +1,9 @@
 package fr.eni.servlets;
 
+import fr.eni.bll.UtilisateurManager;
+import fr.eni.bo.Utilisateur;
+import org.apache.tomcat.util.modeler.Util;
+
 import javax.servlet.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
@@ -7,8 +11,15 @@ import java.io.IOException;
 
 @WebServlet(name = "ServletEditProfile", value = "/editprofile")
 public class ServletEditProfile extends HttpServlet {
+    private static final UtilisateurManager utilisateurManager = new UtilisateurManager();
+
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        int idUser = 2;// TODO : Gestion dynamique à mettre en place avec la navigation
+        Utilisateur lUtilisateur = utilisateurManager.getUserById(idUser);
+
+        request.setAttribute("userInfo", lUtilisateur);
+
         RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/editprofile.jsp");
         rd.forward(request, response);
     }
@@ -16,5 +27,6 @@ public class ServletEditProfile extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
+        doGet(request, response);
     }
 }
