22d2d22411bedbc121dfc17169888fbe27ea0717
RaphaelStAlme <raphael_35_974@outlook.fr>
2021-09-09 16:44:23 +0200

    Revert "feat: Added possibility for user to be authenticated after sign in."
    This reverts commit 6e59e6b0154eb2590bf8b82d9f55cd6f416c4777.

--- src/main/java/fr/eni/servlets/ServletLogin.java
+++ src/main/java/fr/eni/servlets/ServletLogin.java
@@ -1,14 +1,11 @@
 package fr.eni.servlets;
 
-import fr.eni.bll.UtilisateurManager;
-
-import javax.security.sasl.AuthenticationException;
 import javax.servlet.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
 import java.io.IOException;
 
-@WebServlet(value = "/login")
+@WebServlet(name = "ServletLogin", value = "/login")
 public class ServletLogin extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
@@ -18,19 +15,6 @@ public class ServletLogin extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        String pseudo = request.getParameter("pseudo");
-        String password = request.getParameter("password");
 
-        UtilisateurManager utilisateurManager = new UtilisateurManager();
-        boolean isAuthenticated = utilisateurManager.authenticateUser(pseudo, password);
-        if(isAuthenticated){
-            RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/vendreObjet.jsp");
-            System.out.println("User is connected");
-            request.getSession().setAttribute("userIsAuthenticated", true);
-            rd.forward(request, response);
-        } else {
-            request.getSession().setAttribute("userIsAuthenticated", false);
-            System.out.println("User is not connected");
-        }
     }
 }
