73b200617f324045548a5d22a8213e0e8b376018
RaphaelStAlme <raphael_35_974@outlook.fr>
2021-09-09 16:14:49 +0200

    feat: Added possibility for user to be authenticated after sign in. (+3 squashed commit)
    Squashed commit:
    
    [abbe13a] rebase: dev in feature/1001_login
    Resolve confict :
    
    [7735ae5] feat: Added login interface for future users.
    
    [8fcacc6] feat: Added login interface for future users.

--- src/main/java/fr/eni/servlets/ServletLogin.java
+++ src/main/java/fr/eni/servlets/ServletLogin.java
@@ -1,11 +1,14 @@
 package fr.eni.servlets;
 
+import fr.eni.bll.UtilisateurManager;
+
+import javax.security.sasl.AuthenticationException;
 import javax.servlet.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
 import java.io.IOException;
 
-@WebServlet(name = "ServletLogin", value = "/login")
+@WebServlet(value = "/login")
 public class ServletLogin extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
@@ -15,6 +18,19 @@ public class ServletLogin extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String pseudo = request.getParameter("pseudo");
+        String password = request.getParameter("password");
 
+        UtilisateurManager utilisateurManager = new UtilisateurManager();
+        boolean isAuthenticated = utilisateurManager.authenticateUser(pseudo, password);
+        if(isAuthenticated){
+            RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/vendreObjet.jsp");
+            System.out.println("User is connected");
+            request.getSession().setAttribute("userIsAuthenticated", true);
+            rd.forward(request, response);
+        } else {
+            request.getSession().setAttribute("userIsAuthenticated", false);
+            System.out.println("User is not connected");
+        }
     }
 }
