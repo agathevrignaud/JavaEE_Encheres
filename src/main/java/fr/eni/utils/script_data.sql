d82cad1f2585c00a8d365b65e05562505f5f9511
agathevrignaud <vrignaud.agathe@gmail.com>
2021-09-09 10:54:27 +0200

    Modified scripts + added redirection when deleting profile

--- src/main/java/fr/eni/utils/script_data.sql
+++ src/main/java/fr/eni/utils/script_data.sql
@@ -4,7 +4,7 @@ INSERT INTO UTILISATEURS(pseudo,nom,prenom,email,telephone,rue,code_postal,ville
 ('elaunay','Launay', 'Ewen','testE@gmail.com', '0123456789', 'rue truc', '35000', 'Rennes', 'Testing123!');
 
 INSERT INTO UTILISATEURS(pseudo, nom, prenom, email, telephone, rue, code_postal, ville, mot_de_passe, administrateur) VALUES
-('admin','admin', 'admin', 'email', '0123456789', 'rue truc', '35000', 'Rennes', 'mdp', 1);
+('admin','admin', 'admin', 'admin@email.com', '0123456789', 'rue truc', '35000', 'Rennes', 'mdp', 1);
 
 INSERT INTO CATEGORIES VALUES
 ('Informatique'),
