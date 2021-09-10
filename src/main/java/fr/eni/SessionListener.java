eef200965fd6acb251c6ec7d0c6ca77915a7158b
RaphaelStAlme <raphael_35_974@outlook.fr>
2021-09-10 12:24:09 +0200

    feat: Added SessionListener in order to add session timeout by user inactivity.

---
+++ src/main/java/fr/eni/SessionListener.java
@@ -0,0 +1,21 @@
+package fr.eni;
+
+import javax.servlet.annotation.WebListener;
+import javax.servlet.http.HttpSession;
+import javax.servlet.http.HttpSessionEvent;
+import javax.servlet.http.HttpSessionListener;
+
+@WebListener
+public class SessionListener implements HttpSessionListener {
+
+    @Override
+    public void sessionCreated(HttpSessionEvent se) {
+        HttpSession session = se.getSession();
+        session.setMaxInactiveInterval(5*60);
+    }
+
+    @Override
+    public void sessionDestroyed(HttpSessionEvent se) {
+        System.out.println("destroyed");
+    }
+}
--- src/main/java/fr/eni/servlets/ServletLogin.java
+++ src/main/java/fr/eni/servlets/ServletLogin.java
@@ -2,10 +2,13 @@ package fr.eni.servlets;
 
 import fr.eni.bll.UtilisateurManager;
 
-import javax.security.sasl.AuthenticationException;
-import javax.servlet.*;
-import javax.servlet.http.*;
-import javax.servlet.annotation.*;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 @WebServlet(value = "/login")
@@ -20,16 +23,19 @@ public class ServletLogin extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String pseudo = request.getParameter("pseudo");
         String password = request.getParameter("password");
-
         UtilisateurManager utilisateurManager = new UtilisateurManager();
         RequestDispatcher rd;
         boolean isAuthenticated = utilisateurManager.authenticateUser(pseudo, password);
-        if(isAuthenticated){
-            rd = request.getRequestDispatcher("/WEB-INF/home.jsp");
-            request.getSession().setAttribute("userIsAuthenticated", true);
-            request.getSession().setAttribute("authenticationError", false);
 
+        if (isAuthenticated) {
+            rd = request.getRequestDispatcher("");
+
+            HttpSession session = request.getSession(true);
+
+            session.setAttribute("userIsAuthenticated", true);
+            session.setAttribute("authenticationError", false);
             rd.forward(request, response);
+
         } else {
             request.getSession().setAttribute("authenticationError", true);
             doGet(request, response);
