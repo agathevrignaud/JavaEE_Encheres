3c974b2ab5d2b8f6a489b27c0ead220578297106
agathevrignaud <vrignaud.agathe@gmail.com>
2021-09-11 00:40:12 +0200

    Started working on error codes for servlets

--- src/main/java/fr/eni/servlets/CodesResultatServlets.java
+++ src/main/java/fr/eni/servlets/CodesResultatServlets.java
@@ -2,9 +2,9 @@ package fr.eni.servlets;
 
     // 30000 et 39999
     public abstract class CodesResultatServlets {
-
+    // Error Codes for Accounts/Users
+        //  Error Codes for Account Creation
         public static final int CREATE_ACCOUNT_ERROR=30000;
-
         public static final int USERNAME_REQUIRED=30003;
         public static final int SURNAME_REQUIRED=30004;
         public static final int FIRSTNAME_REQUIRED=30005;
@@ -14,11 +14,20 @@ package fr.eni.servlets;
         public static final int CITY_REQUIRED=30009;
         public static final int PWD_REQUIRED=30010;
         public static final int PWD_CONFIRMED_REQUIRED=30010;
-
         public static final int USERNAME_ALREADY_USED=30011;
         public static final int EMAIL_ALREADY_USED=30012;
-
         public static final int PWD_NOT_VALID=30013;
         public static final int PWD_PWD_CONFIRMED_NOT_IDENTICAL=30014;
+
+        // Error Codes for Login
+        // Error Codes for Profile Edition
+
+    // Error Codes for Auctions
+        // Error Codes for Auction Creation
+        // Error Codes for Auction Edition
+
+    // Error Codes for Administration Pannel
+        // Error Codes for User Management
+        // Error Codes for Category Management
 
     }
