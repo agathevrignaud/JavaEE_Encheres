c37246c84c51dc39ec9872d2f5f2767934d1a787
AD\elaunay2021 <ewen.launay2021@campus-eni.fr>
2021-09-13 15:11:40 +0200

    Added constructor to Retrait.java

--- src/main/java/fr/eni/bo/Retrait.java
+++ src/main/java/fr/eni/bo/Retrait.java
@@ -16,6 +16,12 @@ public class Retrait {
         this.ville = ville;
     }
 
+    public Retrait(String rue, String codePostal, String ville) {
+        this.rue = rue;
+        this.codePostal = codePostal;
+        this.ville = ville;
+    }
+
     public int getNo_article() {
         return no_article;
     }
